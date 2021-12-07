1b6ee73576defee67be3fc5bbfe0df0083715fe8
Javier Gomez <jabdeel@hotmail.com>
2021-12-07 13:34:40 -0600

    adding header component and links to thome, shop page and contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import Header from './components/header/Header';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop';
 
@@ -9,6 +10,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
