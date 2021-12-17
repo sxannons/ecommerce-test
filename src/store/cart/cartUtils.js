0b8700d78f0467653fc875781073f08d7ff258c9
Javier Gomez <jabdeel@hotmail.com>
2021-12-17 12:18:42 -0600

    removed comments for previous removeItemFromCart function variations

--- src/store/cart/cartUtils.js
+++ src/store/cart/cartUtils.js
@@ -8,17 +8,6 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
 
-// export const removeItemFromCart = (cartItems, cartItemToRemove) => {
-//   const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
-
-//   if (existingCartItem && existingCartItem.quantity > 1) {
-//     return cartItems.map((cartItem) => (cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem));
-//   }
-// };
-// export const removeItemFromCart = (cartItems, cartItemToRemove) => {
-//   return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
-// };
-
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
 
